7374670d3687893808af4829b05bc73a1e9218a5
Wesley Roberts <wesleyroberts.office@gmail.com>
2024-07-24 17:11:15 -0300

    Adciona Classe Consumer

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/kafka/consumer/SagaOrchestratorConsumer.java
@@ -0,0 +1,55 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.kafka.consumer;
+
+import br.com.microservices.orchestrated.orchestratorservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class SagaOrchestratorConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.start-saga}"
+    )
+    public void cusomeStartSagaEvent(String payload){
+        log.info("Receiving event {} from start-saga topic",payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.orchestrator}"
+    )
+    public void cusomeOrchestratorEvent(String payload){
+        log.info("Receiving event {} from orchestrator topic",payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.finish-success}"
+    )
+    public void cusomeSuccessEvent(String payload){
+        log.info("Receiving success event {} from finish-success topic",payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.finish-fail}"
+    )
+    public void cusomeFailEvent(String payload){
+        log.info("Receiving rollback event {} from finish-fail topic",payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+}
---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/kafka/consumer/PaymentConsumer.java
@@ -0,0 +1,34 @@
+package br.com.microservices.orchestrated.paymentservice.core.kafka.consumer;
+
+import br.com.microservices.orchestrated.paymentservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class PaymentConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.payment-success}"
+    )
+    public void consumeSuccessEvent(String payload){
+        log.info("Receiving success event {} from payment-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.payment-fail}"
+    )
+    public void consumeFailEvent(String payload){
+        log.info("Receiving rollback event {} from payment-fail topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+}
