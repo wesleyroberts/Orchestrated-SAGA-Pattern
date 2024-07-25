9e15f9ef33637c91cbc4f831ecc042d59e47658a
Wesley Roberts <wesleyroberts.office@gmail.com>
2024-07-25 13:28:04 -0300

    Cria DTO's e camda repository do order service.

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,8 +14,10 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Document(collation = "event")
 public class Event {
 
+    @Id
     private String id;
     private String transactionId;
     private String orderId;
---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package br.com.microservices.orchestrated.orderservice.core.repository;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface EventRepository extends MongoRepository<Event,String> {
+}
